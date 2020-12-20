dc5cc55a1efafffcb04ab2efbe04c579efba2145
lichangh20 <74804797+lichangh20@users.noreply.github.com>
2020-12-21 01:49:00 +0800

    魔方debug程序修复

--- 魔方debug.cpp
+++ 魔方debug.cpp
@@ -235,6 +235,7 @@ void (*op[12])() = { up_ckw, left_ckw, front_ckw, right_ckw, back_ckw, down_ckw,
 
 int main()
 {  
+    int num1 = 10;
     int num[12] = { 0 };
     srand((unsigned)time(NULL));
     for (int i = 0; i < 12; i++)
@@ -256,13 +257,28 @@ int main()
     for (int i = 0; i < 12; i++)
         for (int j = 0; j < num[i]; j++)
             (*op[i])();
-    dbg_show();
+    int a[6] = {2,4,1,3,0,5};
     for (int i = 0; i < 6; i++)
     {
         for (int j = 0; j < 3; j++)
             for (int k = 0; k < 3; k++)
-                cout << cube[i][j][k]<<" ";
+            {
+                cout << cube[a[i]][j][k] << " ";
+            }
         cout << endl;
     }
+
+    cout << endl;
     return 0;
 }
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
